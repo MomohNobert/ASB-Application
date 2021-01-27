a8e8da80769601a5e5d899e99879b68fce6fa7a8
Momoh Nobert <okhai0112@gmail.com>
2021-01-27 16:55:06 +0100

    modified table component and styling

---
+++ src/app/components/Table/table.component.jsx
@@ -0,0 +1,11 @@
+import { TableContainer } from "./table.styles"
+
+const Table = () => {
+    return (
+        <TableContainer>
+            Table
+        </TableContainer>
+    )
+}
+
+export default Table;
---
+++ src/app/components/Table/table.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const TableContainer = styled.div`
+    height: 62.5vh;
+    width: 1000px;
+    border: 1px solid red;
+`
