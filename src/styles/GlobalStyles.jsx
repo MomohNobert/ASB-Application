939d87429ecb0f4f51f5f3393c49e173ca43d2bc
Momoh Nobert <okhai0112@gmail.com>
2021-01-27 15:40:03 +0100

    added global styles file

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    border: 0;
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+    outline: none;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  body {
+    background-color: white;
+    width: 100vw;
+    overflow-x: hidden;
+    font-family: Rubik;
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: black;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: white;
+    border-radius: 10%;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: green;
+  }
+`;
