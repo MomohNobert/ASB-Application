07825f0916eaa5107f99ca95a3c16bc37ada4689
Momoh Nobert <okhai0112@gmail.com>
2021-01-27 16:39:29 +0100

    created cards component and styling

---
+++ src/app/components/Cards/cards.component.jsx
@@ -0,0 +1,9 @@
+import { CardsContainer } from "./cards.styles"
+
+const Cards = () => {
+    return (
+        <CardsContainer></CardsContainer>
+    )
+}
+
+export default Cards
---
+++ src/app/components/Cards/cards.styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const CardsContainer = styled.div`
+    height: 106px;
+    width: 100%;
+    margin-bottom: 15px;
+    border: 1px solid red;
+`;
