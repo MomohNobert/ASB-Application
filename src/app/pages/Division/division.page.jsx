57ec23e0f62c2dbecaff0485b2f7c34ab16a9cde
Momoh Nobert <okhai0112@gmail.com>
2021-01-27 16:54:50 +0100

    modified division page and styling

--- src/app/pages/Division/division.page.jsx
+++ src/app/pages/Division/division.page.jsx
@@ -1,9 +1,14 @@
+import Cards from "../../components/Cards/cards.component"
+import Sidebar from "../../components/Sidebar/sidebar.component"
+import Table from "../../components/Table/table.component"
 import { DivisionContainer } from "./division.styles"
 
 const DivisionPage = () => {
     return (
         <DivisionContainer>
-            DivisionPage
+            <Cards />
+            <Sidebar />
+            <Table />
         </DivisionContainer>
     )
 }
--- src/app/pages/Division/division.styles.jsx
+++ src/app/pages/Division/division.styles.jsx
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const DivisionContainer = styled.div`
     height: 100%;
     width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
 `;
