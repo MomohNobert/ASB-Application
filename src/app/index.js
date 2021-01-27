d90661ae939e1d6278324155e00d9f1ef373d9bd
Momoh Nobert <okhai0112@gmail.com>
2021-01-27 16:07:19 +0100

    added index routing and styles

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { GlobalStyle } from '../styles/GlobalStyles';
+import { ContentContainer, HeaderContainer } from './index.styles';
+import DivisionPage from './pages/Division/division.page';
+import Header from './shared/Header/header.component';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
+      <ContentContainer>
+        <Switch>
+          <Route exact path="/" component={DivisionPage} />
+        </Switch>
+      </ContentContainer>
+    </BrowserRouter>
   );
 }
 
---
+++ src/app/index.styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ContentContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    margin-top: 102px;
+`
+export const HeaderContainer = styled.div`
+    position: fixed;
+    width: 100%;
+    top: 0;
+`
