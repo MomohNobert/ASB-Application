f6381c33a8ee09e739b2040f280782f2fb805613
Momoh Nobert <okhai0112@gmail.com>
2021-01-27 15:36:35 +0100

    header component and styles files

---
+++ src/app/shared/Header/header.component.jsx
@@ -0,0 +1,11 @@
+import { HeaderStyle } from "./header.styles"
+
+const Header = () => {
+    return (
+        <HeaderStyle>
+            Header
+        </HeaderStyle>
+    )
+}
+
+export default Header;
---
+++ src/app/shared/Header/header.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderStyle = styled.header`
+    height: 52px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
