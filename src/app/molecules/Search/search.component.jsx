22291a13fdce763b3ae77117fb4109218e52c226
MomohNobert <okhai0112@gmail.com>
2021-01-28 03:39:28 +0100

    created search component

---
+++ src/app/molecules/Search/search.component.jsx
@@ -0,0 +1,13 @@
+import {  SearchContainer, InputContainer } from './search.styles'
+import SearchImg from './assets/search.svg'
+
+const Search = () => {
+    return (
+        <SearchContainer>
+            <InputContainer />
+            <img src={SearchImg} alt="Search" />
+        </SearchContainer>
+    )
+}
+
+export default Search;
---
+++ src/app/molecules/Search/search.styles.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const SearchContainer = styled.div`
+    height: 100%;
+    background: #FFFFFF;
+    display: grid;
+    margin-left: 30px;
+    place-items: center;
+    position: relative;
+
+    img {
+        position: absolute;
+        left: 10px;
+    }
+`;
+
+export const InputContainer = styled.input`
+    width: 176px;
+    height: 36px;
+    border: 1px solid #DCE1E7;
+    border-radius: 4px;
+    padding: 0;
+    padding-left: 30px;
+`
