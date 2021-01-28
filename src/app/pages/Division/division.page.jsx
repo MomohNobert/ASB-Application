00dc7aef9324c007c1620239c36e07016d458991
MomohNobert <okhai0112@gmail.com>
2021-01-28 03:40:19 +0100

    adjustments to division page

--- src/app/pages/Division/division.page.jsx
+++ src/app/pages/Division/division.page.jsx
@@ -1,14 +1,18 @@
 import Cards from "../../components/Cards/cards.component"
 import Sidebar from "../../components/Sidebar/sidebar.component"
 import Table from "../../components/Table/table.component"
-import { DivisionContainer } from "./division.styles"
+import { DivisionContainer, CardsContainer, ContentContainer } from "./division.styles"
 
 const DivisionPage = () => {
     return (
         <DivisionContainer>
-            <Cards />
-            <Sidebar />
-            <Table />
+            <CardsContainer>
+                <Cards />
+            </CardsContainer>
+            <ContentContainer>
+                <Sidebar />
+                <Table />
+            </ContentContainer>
         </DivisionContainer>
     )
 }
--- src/app/pages/Division/division.styles.jsx
+++ src/app/pages/Division/division.styles.jsx
@@ -2,9 +2,21 @@ import styled from 'styled-components'
 
 export const DivisionContainer = styled.div`
     height: 100%;
-    width: 100%;
+    width: calc(100% - 36px);
+    margin: auto;
     display: flex;
-    flex-wrap: wrap;
+    flex-direction: column;
     align-items: center;
     justify-content: space-between;
 `;
+
+export const CardsContainer = styled.div`
+    width: 100%;
+`
+
+export const ContentContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
