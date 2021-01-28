2c78ad24bdf2a0cedec2d77ace4291c8d940619c
MomohNobert <okhai0112@gmail.com>
2021-01-28 03:41:25 +0100

    created typography

---
+++ src/styles/Typography.jsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+
+export const LogoFont = styled.span`
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 22px;
+    color: #000000;
+`
+
+export const MenuFont = styled.span`
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 22px;
+    color: #778699;
+`
+
+export const CHFont = styled.h2`
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 44px;
+`
+
+export const CDFont = styled.p`
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 36px;
+    color: #9696A0;
+`
+
+export const SBHFont = styled.h1`
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 150%;
+`
+
+export const SBCFont = styled.p`
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 140%;
+    color: #0F1A4F;
+`
+
+export const TTFont = styled.h2`
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 15px;
+    text-transform: uppercase;
+    color: #899198;
+`
+
+export const TNFont = styled.p`
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 18px;
+    color: #25213B;
+`
+
+export const TLSFont = styled.p`
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 15px;
+    color: #25213B;
+` 
+
+export const TLAFont = styled.p`
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 15px;
+    color: #878592;
+`
+
+export const TETFont = styled.p`
+    font-size: 12px;
+    line-height: 15px;
+    color: #25213B;
+    font-weight: 500;
+`
+
+export const TECFont = styled.p`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 15px;
+    color: #878592;
+`
