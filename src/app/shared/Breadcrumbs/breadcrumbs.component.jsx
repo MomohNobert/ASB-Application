d95d6b0b06ac0adca6f54ba0029b72f98fe8dc50
MomohNobert <okhai0112@gmail.com>
2021-01-28 03:40:41 +0100

    adjustments to breadcrumps component

--- src/app/shared/Breadcrumbs/breadcrumbs.component.jsx
+++ src/app/shared/Breadcrumbs/breadcrumbs.component.jsx
@@ -1,9 +1,15 @@
-import { BCContainer } from "./breadcrumbs.styles"
+import { MenuFont } from "../../../styles/Typography"
+import { BCContainer, BCContent } from "./breadcrumbs.styles"
+import ArrowImg from './assets/arrow.svg'
 
 const Breadcrumbs = () => {
     return (
         <BCContainer>
-            BreadCrumbs
+            <BCContent>
+                <MenuFont>Divisions</MenuFont>
+                <img src={ArrowImg} alt="Arrow" />
+                <MenuFont>Module</MenuFont>
+            </BCContent>
         </BCContainer>
     )
 }
--- src/app/shared/Breadcrumbs/breadcrumbs.styles.jsx
+++ src/app/shared/Breadcrumbs/breadcrumbs.styles.jsx
@@ -2,5 +2,15 @@ import styled from 'styled-components';
 
 export const BCContainer = styled.div`
     height: 50px;
-    width: 100%''
+    width: calc(100% - 36px);
+    margin: auto;
+    display: flex;
+    align-items: center;
 `; 
+
+export const BCContent = styled.div`
+    display: flex;
+    width: 135px;
+    justify-content: space-around;
+    margin-left: 15px;
+`
